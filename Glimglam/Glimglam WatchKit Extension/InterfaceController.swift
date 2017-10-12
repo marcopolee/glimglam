f778fc07ba0a029ea4b6eb91d1c30beddd292b54
Tyrone Trevorrow <tyrone@sudeium.com>
2017-10-12 22:28:17 +1100

    feat: ios + watchos project skeleton

---
+++ Glimglam/Glimglam WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Glimglam WatchKit Extension
+//
+//  Created by Tyrone Trevorrow on 12/10/17.
+//  Copyright © 2017 Tyrone Trevorrow. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ Glimglam/Glimglam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Glimglam
+//
+//  Created by Tyrone Trevorrow on 12/10/17.
+//  Copyright © 2017 Tyrone Trevorrow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
