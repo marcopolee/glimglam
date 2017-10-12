63f1fcc33d5c4e75785b255accfd32fe7d17da8d
Tyrone Trevorrow <tyrone@sudeium.com>
2017-10-12 22:56:24 +1100

    feat(GlimglamCore): add cross-platform core framework

--- Glimglam/Glimglam WatchKit Extension/InterfaceController.swift
+++ Glimglam/Glimglam WatchKit Extension/InterfaceController.swift
@@ -3,7 +3,7 @@
 //  Glimglam WatchKit Extension
 //
 //  Created by Tyrone Trevorrow on 12/10/17.
-//  Copyright © 2017 Tyrone Trevorrow. All rights reserved.
+//  Copyright © 2017 Marco Polee. All rights reserved.
 //
 
 import WatchKit
--- Glimglam/Glimglam/ViewController.swift
+++ Glimglam/Glimglam/ViewController.swift
@@ -3,10 +3,11 @@
 //  Glimglam
 //
 //  Created by Tyrone Trevorrow on 12/10/17.
-//  Copyright © 2017 Tyrone Trevorrow. All rights reserved.
+//  Copyright © 2017 Marco Polee. All rights reserved.
 //
 
 import UIKit
+import GlimglamCore
 
 class ViewController: UIViewController {
 
