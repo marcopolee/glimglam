b054ad2b593b08f8a283f913d0b9d51a490f7fbf
Tyrone Trevorrow <tyrone@sudeium.com>
2017-10-15 07:44:33 +1100

    style: reorganise folders

---
+++ Glimglam/Secrets.swift
@@ -0,0 +1,17 @@
+//
+//  Secrets.swift
+//  Glimglam
+//
+//  Created by Tyrone Trevorrow on 12/10/17.
+//  Copyright © 2017 Marco Polee. All rights reserved.
+//
+
+import Foundation
+
+enum Secrets {
+    enum GitLab {
+        static let ApplicationSecret = "1b4c6b5a1e439ff7f3ddd0e397345eb4b182aa1e1c0ef63a37be319bd88ccb55"
+        static let RedirectURI = "glimglametdgbymg://gitlab/login/redirect"
+        static let ClientId = "edded319536a9b6d990c70f4c4ea79135aa9a9006da66314b46d18a48e42dd66"
+    }
+}
